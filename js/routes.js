be0f4d109da0c8209170e5f52e265e2ceac56914
Virginia Majuelos <virginia.majuelos@gmail.com>
2023-01-05 14:07:01 +0100

    first commit

---
+++ js/routes.js
@@ -0,0 +1,76 @@
+const PATHS = {
+  home: {
+    path: "/",
+    template: `
+    <!-- Sección Home -->
+    <section class="section-home">
+    <h1 class="section-home__title">Elena Picanço</h1>
+    <p class="section-home__subtitle">Cantante / Trompetista </p>
+    <hr>
+    <img class="section-home__imagen" src="../assets/images/hero-banner_desktop.jpg">
+    </section>
+
+    `,
+  },
+
+  biografia: {
+    path: "/biografia",
+    template: `
+    <section class="section-biografia">
+    <h2 class="heading_h2">Sobre mi.</h2>
+    <hr>
+    <p class="text">
+          Nacida en mayo de 1996, andaluza de cuna y exploradora de otras
+          ciudades. Graduada en Enseñanzas Artísticas Musicales en
+          Interpretación-trompeta por el Conservatorio Superior de Música
+          «Manuel Castillo» de Sevilla en junio de 2019. En septiembre de 2017
+          viaja a Bélgica, al Koninklijk Conservatorium Brussel, para completar
+          el curso con el trompetista Manu Mellaerts. Motivada por un
+          crecimiento personal y profesional.
+        </p>
+        <p class="text">
+          Ambiciosa por aprender todo lo que pueda de la forma que sea.
+          Enriqueciéndose de la sabiduría popular y estudiosos, «gente que se
+          aproxime a tu forma de ver la vida, experiencias, que te haga crecer y
+          ser mejor».
+        </p>
+        <p class="text">
+          Hace 20 años que empecé en el mundo de la música y he podido formar
+          parte de grupos musicales, orquestas y bandas, actuando en diferentes
+          espectáculos como cantante, corista e instrumentista. Además, de
+          trabajar con niños en escuelas de música y como profesora particular
+          de instrumento.
+        </p>
+
+        <div class="galeria">
+        <img src="assets/images/Galeria 3.jpg" alt="" />
+        <img src="assets/images/Galeria 8.jpg" alt="" />
+        <img src="assets/images/Galeria 4.jpg" alt="" />
+        <img src="assets/images/Galeria 5.jpg" alt="" />
+        
+      </div>
+    </section>
+    `,
+  },
+  proyectos: {
+    path: "/proyectos",
+    template: `
+        <h1>proyectos</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas. Diam vulputate ut pharetra sit. Egestas dui id ornare arcu. Non odio euismod lacinia at quis risus sed vulputate odio. Pharetra vel turpis nunc eget lorem dolor sed. Arcu non sodales neque sodales ut etiam sit. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Amet commodo nulla facilisi nullam. Dignissim convallis aenean et tortor. Sem fringilla ut morbi tincidunt augue interdum velit. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Proin nibh nisl condimentum id venenatis. Velit ut tortor pretium viverra suspendisse potenti nullam ac.</p>    
+        `,
+  },
+  galeria: {
+    path: "/galeria",
+    template: `
+        <h1>galeria</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas. Diam vulputate ut pharetra sit. Egestas dui id ornare arcu. Non odio euismod lacinia at quis risus sed vulputate odio. Pharetra vel turpis nunc eget lorem dolor sed. Arcu non sodales neque sodales ut etiam sit. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Amet commodo nulla facilisi nullam. Dignissim convallis aenean et tortor. Sem fringilla ut morbi tincidunt augue interdum velit. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Proin nibh nisl condimentum id venenatis. Velit ut tortor pretium viverra suspendisse potenti nullam ac.</p>    
+        `,
+  },
+  contacto: {
+    path: "/contacto",
+    template: `
+        <h1>contacto</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas. Diam vulputate ut pharetra sit. Egestas dui id ornare arcu. Non odio euismod lacinia at quis risus sed vulputate odio. Pharetra vel turpis nunc eget lorem dolor sed. Arcu non sodales neque sodales ut etiam sit. Eleifend mi in nulla posuere sollicitudin aliquam ultrices. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Amet commodo nulla facilisi nullam. Dignissim convallis aenean et tortor. Sem fringilla ut morbi tincidunt augue interdum velit. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Proin nibh nisl condimentum id venenatis. Velit ut tortor pretium viverra suspendisse potenti nullam ac.</p>    
+        `,
+  },
+};
